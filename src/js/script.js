374550a54fd1fd226233dce13346115622ed994e
vaiola3 <newillianpereira@gmail.com>
2020-06-16 02:38:10 -0300

    criando single page para pagina pessoal

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+{/* <p>
+              Estudante de Ciência da Computação e entusiasta em aplicações web com 
+              interesse em realizar projetos que contribuam com a comunidade à minha volta.
+              No momento conhecendo o universo do Java Web e experimentando o Node.js no
+              consumo de APIs e pequenas automações.
+              Creio que o maior prazer da vida seja o simples fato de não sabermos tudo.
+            </p> */}
+
+const prateleira = document.getElementById('prateleira');
+
+const height = window.screen.height;
+
+$(prateleira).attr('style', 'height: '+height+'px;')
